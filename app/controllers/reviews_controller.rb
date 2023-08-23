e60def8462366fd8fa36b16dbf6ab8265041bc05
madeleine plumridge <madeleine.plumridge@gmail.com>
2023-08-23 16:27:00 +0200

    fix on new review template

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,8 +4,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show', status: :unprocessable_entity
+    end
   end
 
   def index
